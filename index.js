75d31e5556f3355d67f4fb96231ba66cc959b4fb
Raphaël Vieira <raphael.branco@agence-mint.ch>
2024-06-08 10:24:35 +0200

    4sync

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const ffmpeg = require("fluent-ffmpeg");
 const app = express();
 const PORT = 4000;
 
-const VIDEOS_PATH = "../resources/raw/videos";
-const THUMBNAIL_PATH = "../resources/client/thumbnails";
+const VIDEOS_PATH = "../resources/videos";
 
 app.use(cors());
 
@@ -16,39 +15,57 @@ app.get("/", (_req, _res) => {
 });
 
 app.get("/videos", (_req, _res) => {
-  fs.readdir(VIDEOS_PATH, (err, res) => {
+  fs.readdir(VIDEOS_PATH, (err, files) => {
     if (err) {
       _res.status(500).send("Internal Server Error.");
       return;
     }
 
-    let videosNames = [];
+    const regex = /\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}.mp4/;
+    let videos = [];
 
-    res.forEach((file) => {
-      const regex = /\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}.mp4/;
+    const formatDateComponent = (component) => {
+      return component.toString().padStart(2, '0');
+    };
+
+    files.forEach((file) => {
       if (regex.test(file)) {
-        videosNames.push(file);
+        let videoPath = `${VIDEOS_PATH}/${file}`;
+        let videoServedStaticFile = `/videos/${file}`;
+
+        // Get file creation date
+        fs.stat(videoPath, (err, stats) => {
+          if (err) {
+            _res.status(500).send("Internal Server Error.");
+            return;
+          }
+
+          const birthtime = stats.birthtime;
+          const formattedDate = `${formatDateComponent(birthtime.getDate())}.${formatDateComponent(birthtime.getMonth() + 1)}.${birthtime.getFullYear()} ${formatDateComponent(birthtime.getHours())}:${formatDateComponent(birthtime.getMinutes())}:${formatDateComponent(birthtime.getSeconds())}`;
+
+          // Push formatted date to videos array
+          videos.push({ path: videoServedStaticFile, creationDate: formattedDate });
+
+          // Check if all videos have been processed
+          if (videos.length === files.filter(f => regex.test(f)).length) {
+            _res.status(200).json(videos);
+          }
+        });
       }
     });
 
-    if (videosNames.length === 0) {
+    if (files.filter(f => regex.test(f)).length === 0) {
       _res.status(200).send("No videos found.");
-    } else {
-      videosNames.forEach((videoName) => {
-        const videoPath = `${VIDEOS_PATH}/${videoName}`;
-
-        ffmpeg(videoPath).screenshot({
-          timestamps: ["00:00:05"],
-          filename: `${videoName}.png`,
-          folder: THUMBNAIL_PATH,
-          size: "300x200",
-        });
-      });
-
-      _res.status(200).send(videosNames);
     }
   });
 });
+
+app.get("/videos/:videoName/delete", (_req, _res) => {
+  const videoName = _req.params.videoName;
+  _res.status(200).send(`Video "${videoName}" deleted.`);
+});
+
+app.use("/videos", express.static(VIDEOS_PATH));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} !`);
